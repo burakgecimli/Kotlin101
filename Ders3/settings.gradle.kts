4279b8e15e742dc1466d081891f8abb61b9abf40
burakgecimli <burakgecimli444@gmail.com>
2022-08-06 17:52:13 +0300

    Kotlin101
    Kotlin101

---
+++ Ders3/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Ders3"
+
---
+++ Ders3/src/main/kotlin/1 Getting Started/HelloWorld.kt
@@ -0,0 +1,22 @@
+package `1 Getting Started`
+
+
+fun main() {
+    println("Hello World")
+
+}
+
+/**
+ * Dokümantasyon Yorum Satırı @param args program argüment'lerini tutar.
+ */
+fun main(args: Array<String>) {
+println("Hello World")
+}
+
+// Tekil Yorum Satırı
+
+/*
+  Çoklu Yorum Satırı
+*/
+
+//TODO (Burak) burası daha sonra düzeltilecektir.
